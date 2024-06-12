3a7fead4e1de48bf3f743ac4423a0e744388dbc5
mateuszzolkiewki <mateuszzolkiewski@gmail.com>
2024-06-12 17:18:26 +0200

    dodanie funkcji silnia

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,18 +2,37 @@
 
 using namespace std;
 
+int silnia ();
+
 int main() {
-    int a, b;
-    cout << "Podaj liczbe calkowita ";
-    cin >> a >> b;
     int wyjscie;
     do {
-        cout << endl;
-        cout << endl;
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
         cout << "0. Wyjscie" << endl;
+        cout << "1. Silnia" << endl;
         cin >> wyjscie;
     } while(wyjscie != 0);
+    return 0;
+}
+
+int silnia () {
+    int liczba;
+    cout << "Podaj liczbe: ";
+    cin >> liczba;
+
+    if (liczba < 0) {
+        cout << "Podana liczba jest ujemna";
+        return 0;
+    }
+
+    int wynik = liczba;
+
+    for (int i = liczba - 1; i > 0; i--) {
+        wynik = wynik * i;
+    }
+
+    cout << endl << "Wynik = " << wynik << endl;
+
     return 0;
 }
